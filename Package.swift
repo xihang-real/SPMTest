54efdd49b5ed5e5e4c10620bc2ed3e4c61d70d35
careyxi <careyxi@zego.im>
2023-01-30 19:38:08 +0800

    commit first

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SPMTest",
+    
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "SPMTest", targets: ["SPMTest"])
+    ],
+    
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/zegolibrary/express-video-ios", from: "3.0.0"),
+    ],
+    
+    targets: [
+        .binaryTarget(
+                    name: "ZegoExpressEngine",
+                    url: "https://artifact-node.zego.cloud/generic/swiftpm/public/express-video-ios/ZegoExpressEngine.xcframework.zip?version=3.2.0",
+                    checksum: "bbc5df01b1960cc3790bcaa60ef603188b2415fdb3a293de33918e18b9dd28d5"
+                )
+    ],
+)
