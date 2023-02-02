21cf3a76c04b3a7b1d2d1d91909166e0354ccc09
careyxi <careyxi@zego.im>
2023-02-02 17:29:12 +0800

    update swift package.

--- Package.swift
+++ Package.swift
@@ -4,25 +4,35 @@
 import PackageDescription
 
 let package = Package(
+    name: "SPMTestSDK",
     
-    name: "SPMTest",
-    
-    platforms: [
-        .iOS(.v12)
-    ],
+    platforms: [.iOS(.v12)],
     
     products: [
-        .library(name: "SPMTest", targets: ["SPMTest"])
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SPMTestSDK",
+            targets: ["SPMTestSDK","SPMTest"]),
     ],
     
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/zegolibrary/express-video-ios", from: "3.0.0"),
+        .package(url: "https://github.com/zegolibrary/express-video-ios", from: "3.0.0")
     ],
     
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "SPMTestSDK",dependencies: [
+            .product(name: "ZegoExpressEngine", package: "express-video-ios")]),
+//        .binaryTarget(name: "SPMTest",
+//                      path: "artifacts/SPMTest.xcframework"),
+        
         .binaryTarget(name: "SPMTest", url: "https://github.com/xihang-real/SPMTest/releases/download/v1.1.0/SPMTest.xcframework.zip", checksum: "14481356334f3a16b949a1cc85fd86ac6548ebae3a9766e50c2d967bb4c4766f"),
+        
+//        .testTarget(
+//            name: "SPMTestTests",
+//            dependencies: ["SPMTestSDK"])
     ]
 )
-
---
+++ artifacts/SPMTest.xcframework/ios-arm64_x86_64-simulator/SPMTest.framework/Headers/SPMTest.h
@@ -0,0 +1,18 @@
+//
+//  SPMTest.h
+//  SPMTest
+//
+//  Created by zego on 2023/1/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPMTest.
+FOUNDATION_EXPORT double SPMTestVersionNumber;
+
+//! Project version string for SPMTest.
+FOUNDATION_EXPORT const unsigned char SPMTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPMTest/PublicHeader.h>
+
+
